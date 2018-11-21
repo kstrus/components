0d0fc19304f414c0e46f0e5e543689665610c199
kstrus <katarzyna.anna.strus@gmail.com>
2018-11-21 20:58:57 +0100

    Create posts - naive approach

--- src/index.js
+++ src/index.js
@@ -3,7 +3,34 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     render() {
-        return <div>Hi there from the class!</div>
+        return (
+            <div className="ui container comments">
+                <div className="comment">
+                    <a href="/" className="avatar">
+                        <img alt="avatar"/>
+                    </a>
+                    <div className="content">
+                        <a href="/" className="author">Terry</a>
+                        <div className="metadata">
+                            <span className="date">Today at 6:09 PM</span>
+                        </div>
+                        <div className="text">Nice blog post!</div>
+                    </div>
+                </div>
+                <div className="comment">
+                    <a href="/" className="avatar">
+                        <img alt="avatar"/>
+                    </a>
+                    <div className="content">
+                        <a href="/" className="author">Patti P.</a>
+                        <div className="metadata">
+                            <span className="date">Yesterday at 2:22 AM</span>
+                        </div>
+                        <div className="text">This is awesome</div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
